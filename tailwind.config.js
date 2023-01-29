ec45d933161463748deea9b9d3fea91de07beb42
MD. Faysal Rahman <faysal35-3119@diu.edu.bd>
2023-01-30 00:47:20 +0600

    V:01

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["*"],
+  theme: {
+
+    extend: {
+      colors: {
+        gray: { 800: "#991b1" }
+      },
+      container: {
+        center: true,
+        width: "90%",
+
+      },
+    },
+  },
+  plugins: [],
+}
